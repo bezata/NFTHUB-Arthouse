b8cf744b4a92b07a7e13eeb1ab9de5905b00fc5b
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2023-04-22 17:23:57 +0300

    netlify fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import react from "@astrojs/react";
-// https://astro.build/config
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
+  integrations: [react()]
 });
