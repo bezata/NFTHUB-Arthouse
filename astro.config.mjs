ac762c5636f397aae6bdb0c0e65bbc0a8611cdb8
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2023-04-22 16:54:51 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import react from "@astrojs/react";
 // https://astro.build/config
 import { defineConfig } from "astro/config";
-import vercelServerless from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify/functions";
+
 export default defineConfig({
   output: "server",
-  adapter: vercelServerless(),
+  adapter: netlify(),
 });
