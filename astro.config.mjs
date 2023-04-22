3f4747f4f356d0329f3ba11a4546d3df17bb2989
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2023-04-22 17:25:50 +0300

    tailwind astro integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@ import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import netlify from "@astrojs/netlify/functions";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: netlify(),
-  integrations: [react()]
+  integrations: [react(), tailwind()],
 });
