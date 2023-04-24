65627204671d4c93da62bd748d1886179cc188be
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2023-04-24 16:43:08 +0300

    Update NFTHUB.sol

--- contracts/NFTHUB.sol
+++ contracts/NFTHUB.sol
@@ -8,6 +8,7 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 contract NFTHUB is ReentrancyGuard, ERC721URIStorage {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
+    Counters.Counter private _itemIds;
 
     address payable owner;
     uint256 listingPrice = 1;
@@ -29,8 +30,7 @@ contract NFTHUB is ReentrancyGuard, ERC721URIStorage {
     mapping(uint256 => Item) private _idToItem;
 
     event ItemListed(uint256 indexed id, string name, string tokenURI);
-
-    /**
+     /**
         * @dev Validation checks are performed to ensure validity of the input data
         * @notice Allow users to list and mint an NFT
         * @param name The name of the NFT
@@ -46,7 +46,7 @@ contract NFTHUB is ReentrancyGuard, ERC721URIStorage {
         require(bytes(name).length > 0, "Empty name");
         require(bytes(description).length > 0, "Empty description");
         require(bytes(tokenURI).length > 0, "Empty token URI");
-        
+
         uint256 tokenId = _tokenIds.current();
 		_tokenIds.increment();
 
@@ -54,8 +54,12 @@ contract NFTHUB is ReentrancyGuard, ERC721URIStorage {
         _safeMint(msg.sender, tokenId);
         _setTokenURI(tokenId, tokenURI);
 
+        // Create a new item
+        _itemIds.increment();
+        uint256 itemId = _itemIds.current();
+
         Item memory newItem = Item(
-            tokenId,
+            itemId,
             msg.sender,
             address(this),
             tokenId,
@@ -63,18 +67,16 @@ contract NFTHUB is ReentrancyGuard, ERC721URIStorage {
             description,
             tokenURI
         );
-        _idToItem[tokenId] = newItem;
+        _idToItem[itemId] = newItem;
 
-        emit ItemListed(tokenId, name, tokenURI);
+        emit ItemListed(itemId, name, tokenURI);
         payable(owner).transfer(listingPrice);
     }
-
-    /// @return listingPrice the price to list an NFT 
+    /// @return listingPrice the price to list an NFT
     function getListingPrice() public view returns (uint256) {
         return listingPrice;
     }
-
-    /**
+     /**
         * @dev Callable only by deployer/owner
         * @notice Allows the deployer/owner of the smart contract to update the listing price
         * @param newPrice The new value for the listing price
@@ -84,9 +86,7 @@ contract NFTHUB is ReentrancyGuard, ERC721URIStorage {
         listingPrice = newPrice;
     }
 
-    function fetchItem(
-        uint256 tokenId
-    )
+    function fetchItem(uint256 itemId)
         public
         view
         returns (
@@ -99,9 +99,9 @@ contract NFTHUB is ReentrancyGuard, ERC721URIStorage {
             string memory
         )
     {
-        require(_idToItem[tokenId].minter != address(0), "Item does not exist");
+        require(_idToItem[itemId].minter != address(0), "Item does not exist");
 
-        Item storage item = _idToItem[tokenId];
+        Item storage item = _idToItem[itemId];
 
         return (
             item.id,
@@ -112,5 +112,14 @@ contract NFTHUB is ReentrancyGuard, ERC721URIStorage {
             item.description,
             item.tokenURI
         );
+    }
+    // Fetch the data for ui
+    function getAllItems() public view returns (Item[] memory) {
+        Item[] memory items = new Item[](_itemIds.current());
+        for (uint256 i = 1; i <= _itemIds.current(); i++) {
+            Item storage item = _idToItem[i];
+            items[i - 1] = item;
+        }
+        return items;
     }
 }
