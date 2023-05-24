024f5458dd6c51f2a7975419840a1682730bb1ef
Behzat Can Acele <61169260+bezata@users.noreply.github.com>
2023-05-24 16:08:40 +0300

    Update Dropzone.js

--- src/web3stuff/Dropzone.js
+++ src/web3stuff/Dropzone.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-const pinataApiKey = "37349a08b8489b950b38";
+const pinataApiKey = "9c6cb7176d7eb0e15192";
 const pinataSecretApiKey =
-  "f9c107f9b3be86eded9f0acba053effd8318bd98d87d48dedd97ea3cc36781fb";
+  "369e3262bcaed055ef60a7a447a29df3cb49186cb63908f8b227e350e13c5805";
 const pinataBaseURL = "https://api.pinata.cloud/pinning";
 
 export const UploadFileToIPFS = async (file) => {
